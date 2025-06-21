e0d182e300fb62e448c4abd14d4b57268bd4ff34
Neha <nehatualea@gmail.com>
2025-06-21 19:17:41 +0530

    Tried consoling useState.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import Header from "./Assignments/03-Assignment/Header/Header.jsx";
+import { useState } from "react";
 
 const title = React.createElement(
-    "h1",
-    {id:"title"},
-    "Hello World from React"
-)
+  "h1",
+  { id: "title" },
+  "Hello World from React"
+);
 
 // JSX (transpiled to React.createElement) => ReactElement => Js Object => HTML element render
 
@@ -20,39 +21,34 @@ const title = React.createElement(
 // </h1>)
 // React element
 const headingJSX = (
-
-  <h1  className="heading">
-    This is JSX
-  </h1>
+  <>
+    <h1 className="heading">This is JSX</h1>
+  </>
 );
 // console.log(heading);//object
 
-
 // Functional Component: returns a react element -> JSX
 
-
-
-
-const HeadingComponent2 =() =>{
-    return <h1 id="heading">h1 from functional Component 2</h1>
-}
+const HeadingComponent2 = () => {
+  console.log(useState);
+  return <h1 id="heading">h1 from functional Component 2</h1>;
+};
 
 const data = "This is 100 data from variable";
-const HeadinComponent= () =>{
-    return (
+const HeadinComponent = () => {
+  return (
     <>
-    <h1>{console.log("xbsidoifjdas")}</h1>
-    <h2>{1000+2}</h2>
-    <h3>{data}</h3>
-    {title}
-    <HeadingComponent2></HeadingComponent2>
-   <h1>h1 from functional Component</h1>
-   </>
-    )
-    
-}
+      <h1>{console.log("xbsidoifjdas")}</h1>
+      <h2>{1000 + 2}</h2>
+      <h3>{data}</h3>
+      {title}
+      <HeadingComponent2></HeadingComponent2>
+      <h1>h1 from functional Component</h1>
+    </>
+  );
+};
 console.log(headingJSX); //object
 console.log(title); //object
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<Header/>);
+root.render(<HeadinComponent />);
